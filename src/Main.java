ccb4d0c5826aa71dfd5a5e92f4b09f3396f9e672
Junior Brito <junior.br.pr@gmail.com>
2022-10-19 16:02:35 +0200

    Reorganized code structure

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,29 @@
+
 public class Main {
     public static void main(String[] args) {
-//        Ship aircraftCarrier = new Ship(5);
-//        Ship battleShip = new Ship(4);
-//        Ship submarine = new Ship(3);
-//        Ship cruiser = new Ship(3);
-//        Ship destroyer = new Ship(2);
-        displayField(makeField());
+        Ship aircraftCarrier = new Ship(5);
+        Ship battleShip = new Ship(4);
+        Ship submarine = new Ship(3);
+        Ship cruiser = new Ship(3);
+        Ship destroyer = new Ship(2);
 
+        Battlefield battlefield = new Battlefield(aircraftCarrier, battleShip, submarine, cruiser, destroyer);
+
+        battlefield.displayField();
 
     }
+}
 
-//    private char[][] adjust(Ship aircraftCarrier,Ship battleShip, Ship submarine,Ship cruiser, Ship destroyer){
-//
-//    }
+class Battlefield{
+    private Ship[] ships;
+    private String[][] field;
 
-    public static void displayField(String[][] field) {
-        for (String[] chars : field) {
-            for (int j = 0; j < chars.length; j++) {
-                System.out.print(chars[j] + " ");
-                if (j == chars.length - 1) {
-                    System.out.print("\n");
-                }
-            }
-        }
+    public Battlefield(Ship... ships) {
+        this.ships = ships;
+        this.field = makeField();
     }
 
-    public static String[][] makeField() {
+    private String[][] makeField() {
         final int FIELD_WIDTH = 11;
         final int FIELD_HEIGHT = 11;
         String[][] field = new String[FIELD_HEIGHT][FIELD_WIDTH];
@@ -46,16 +44,48 @@ public class Main {
 
         return field;
     }
+
+    public void displayField() {
+        for (String[] chars : this.field) {
+            for (int j = 0; j < chars.length; j++) {
+                System.out.print(chars[j] + " ");
+                if (j == chars.length - 1) {
+                    System.out.print("\n");
+                }
+            }
+        }
+    }
+
 }
 
 class Ship {
-    private final int length;
+    private int length;
+    private int[] pos1;
+    private int[] pos2;
 
     public Ship(int length) {
         this.length = length;
+        this.pos1 = new int[2];
+        this.pos2 = new int[2];
     }
 
     public int getLength() {
         return length;
+    }
+
+    public int[] getPos1() {
+        return pos1;
+    }
+
+    public void setPos1(int[] pos1) {
+        this.pos1 = pos1;
+    }
+
+    public int[] getPos2() {
+        return pos2;
+    }
+
+    public void setPos2(int[] pos2) {
+        this.pos2 = pos2;
     }
 }
