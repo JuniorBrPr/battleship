be0f9d66031fe0b20f51474836171ec8b55765eb
Junior Brito <junior.br.pr@gmail.com>
2023-02-03 13:39:46 +0100

    Change datatype field

--- src/battleship/Battlefield.java
+++ src/battleship/Battlefield.java
@@ -1,14 +1,14 @@
 package battleship;
 
 public class Battlefield {
-    private String[][] field;
+    private char[][] field;
 
     public Battlefield() {
         this.field = makeField();
     }
 
     protected void displayField() {
-        for (String[] chars : this.field) {
+        for (char[] chars : this.field) {
             for (int j = 0; j < chars.length; j++) {
                 System.out.print(chars[j] + " ");
                 if (j == chars.length - 1) {
@@ -18,33 +18,33 @@ public class Battlefield {
         }
     }
 
-    protected static String[][] makeField() {
+    protected static char[][] makeField() {
         final int FIELD_WIDTH = 11;
         final int FIELD_HEIGHT = 11;
-        final String FOG_CHAR = "~";
-        String[][] field = new String[FIELD_HEIGHT][FIELD_WIDTH];
+        final char FOG_CHAR = '~';
+        char[][] field = new char[FIELD_HEIGHT][FIELD_WIDTH];
 
         for (int i = 0; i < FIELD_HEIGHT; i++) {
             for (int j = 0; j < FIELD_WIDTH; j++) {
                 if (i == 0 && j != 0) {
-                    field[i][j] = Integer.toString(j);
+                    field[i][j] = Integer.toString(j).charAt(0);
                 } else if (i > 0 && j == 0) {
-                    field[i][j] = String.valueOf((char) (i + 64));
+                    field[i][j] = String.valueOf((char) (i + 64)).charAt(0);
                 } else if (i > 0) {
                     field[i][j] = FOG_CHAR;
                 } else {
-                    field[i][j] = " ";
+                    field[i][j] = ' ';
                 }
             }
         }
         return field;
     }
 
-    protected String[][] getField() {
+    protected char[][] getField() {
         return field;
     }
 
-    protected void setField(String[][] field) {
+    protected void setField(char[][] field) {
         this.field = field;
     }
 }
